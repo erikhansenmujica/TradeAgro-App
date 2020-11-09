2ccab69acbe5994a562ebeecb1c25ee9c707c6cd
sebakjo <sebakjolhede@gmail.com>
2020-11-08 23:21:00 -0300

    touchablehighlight > button

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, TouchableHighlight } from "react-native";
 import styles from "../../generalStyles";
-import Buttons from "./buttons.js";
 import mainButtonsInfo from "../../mainButtonsInfo";
 
 export default function ({ navigation }) {
   return (
     <View style={styles.container}>
       {mainButtonsInfo.map((button) => (
-        <Buttons
-          title={button.title}
-          name={button.name}
-          navigation={navigation}
-        ></Buttons>
+        <TouchableHighlight
+          style={styles.buttons}
+          onPress={() => navigation.navigate(button.name)}
+          key={button.name}
+        >
+          <Text>{button.title}</Text>
+        </TouchableHighlight>
       ))}
     </View>
   );
--- generalStyles.js
+++ generalStyles.js
@@ -1,13 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    paddingTop: 60, // ponele
+    flexDirection: "row",
+    justifyContent: "space-around",
+    flexWrap: "wrap",
+    
   },
+  buttons: {},
 });
 
-
-export default styles
+export default styles;
