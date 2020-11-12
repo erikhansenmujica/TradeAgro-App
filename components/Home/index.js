a2144ed745dd9d75b3b361b5c96d9363e59f8b62
erikhansen-ml <ext_ehansen@mercadolibre.com>
2020-11-12 16:51:18 -0300

    custom font

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { Text, View, TouchableHighlight } from "react-native";
+import { View, TouchableHighlight, ImageBackground } from "react-native";
+import { Text } from "../Elements";
 import styles from "../../generalStyles";
 import mainButtonsInfo from "../../mainButtonsInfo";
-
+import background from "../../assets/fondoMovil.png";
 export default function ({ navigation }) {
   return (
-    <View style={styles.container}>
-      {mainButtonsInfo.map((button) => (
-        <TouchableHighlight
-          style={styles.buttons}
-          onPress={() => navigation.navigate(button.name)}
-          key={button.name}
-        >
-          <Text>{button.title}</Text>
-        </TouchableHighlight>
-      ))}
-    </View>
+    <ImageBackground source={background} style={styles.ImageBackground}>
+      <View style={{ ...styles.container, fontFamily: "Roboto-Medium" }}>
+        {mainButtonsInfo.map((button) => (
+          <TouchableHighlight
+            style={styles.buttons}
+            onPress={() => navigation.navigate(button.name)}
+            key={button.name}
+          >
+            <Text content={button.title} />
+          </TouchableHighlight>
+        ))}
+      </View>
+    </ImageBackground>
   );
 }
---
+++ components/OrderForm/OrderFormStyles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  OrderContainer: {
+    height: "55%",
+    width: "75%",
+    borderStyle: "solid",
+    borderColor: "#F4F4F4",
+    borderWidth: 1,
+    borderRadius: 40,
+    backgroundColor: "#fff",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  Title: {
+    fontSize: 35,
+    textAlign: "center",
+    color: "#0061AE",
+    marginTop: 10,
+    fontFamily:"Roboto-Medium",
+  },
+});
