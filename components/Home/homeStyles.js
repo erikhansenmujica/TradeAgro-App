030480967e2e815915a65f57ae4aba7413eb2104
sebakjo <sebakjolhede@gmail.com>
2020-11-12 21:21:16 -0300

    versiones

--- components/Home/homeStyles.js
+++ components/Home/homeStyles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   buttons: {
-    backgroundColor: "beige",
+    backgroundColor: "#fff",
     borderRadius: 20,
     height: "20%",
     width: "34%",
