2ab77d853fa205828f7affc531fa01803b82003b
erikhansen-ml <ext_ehansen@mercadolibre.com>
2020-12-11 11:34:27 -0300

    asd

--- store/constants.js
+++ store/constants.js
@@ -1,3 +1,3 @@
 export const ADD_PRODUCTS = "ADD_PRODUCTS";
 export const ADD_MARKETS = "ADD_MARKETS";
-export const URL = "http://localhost:3000";
+export const URL = "https://tradeagro-api.herokuapp.com";
