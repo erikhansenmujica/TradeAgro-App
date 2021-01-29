d1b24ed771594c7271dadaede6ca918925298faa
erikhansen-ml <ext_ehansen@mercadolibre.com>
2021-01-29 00:55:33 -0300

    asd

--- store/constants.js
+++ store/constants.js
@@ -2,4 +2,4 @@ export const ADD_PRODUCTS = "ADD_PRODUCTS";
 export const ADD_MARKETS = "ADD_MARKETS";
 export const ADD_USER = "ADD_USER";
 export const ADD_NOTIFICATIONS = "ADD_NOTIFICATIONS";
-export const URL = "http://18.224.33.19:3000";
+export const URL = "http://3.21.104.115:3000";
