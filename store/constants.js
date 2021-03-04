cd46c1983f908f620fa4f94c5d71b3d7be3242c1
erikhansenmujica <erikhansenmujica@gmail.com>
2021-03-04 17:39:13 -0300

    asd

--- store/constants.js
+++ store/constants.js
@@ -2,4 +2,4 @@ export const ADD_PRODUCTS = "ADD_PRODUCTS";
 export const ADD_MARKETS = "ADD_MARKETS";
 export const ADD_USER = "ADD_USER";
 export const ADD_NOTIFICATIONS = "ADD_NOTIFICATIONS";
-export const URL = "http://3.21.104.115:3000";
+export const URL = "http://3.142.77.59:3000";
