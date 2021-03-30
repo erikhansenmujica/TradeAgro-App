8ae249dfb41267053c208d9a1913bd7ec386c5c8
sebakjo <sebakjolhede@gmail.com>
2021-03-29 22:32:25 -0300

    market hour

--- store/reducers/marketsReducer.js
+++ store/reducers/marketsReducer.js
@@ -7,8 +7,10 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_MARKETS:
       const markets = action.payload.sort((a, b) => {
-        let dateA = new Date(a.fecha), dateB = new Date(b.fecha)
-        return dateB - dateA;
+        let dateA = new Date(a.fecha),
+          dateB = new Date(b.fecha);
+        if (dateB - dateA == 0) return b.hora - a.hora;
+        else return dateB - dateA;
       });
       return { ...state, all: markets };
 
