1fc97986a06697e86a4448c754c71da68d818253
erikhansenmujica <erikhansenmujica@gmail.com>
2021-03-30 14:20:38 -0300

    no me acuerdo

--- store/reducers/marketsReducer.js
+++ store/reducers/marketsReducer.js
@@ -6,12 +6,13 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_MARKETS:
-      const markets = action.payload.sort((a, b) => {
-        let dateA = new Date(a.fecha),
-          dateB = new Date(b.fecha);
-        if (dateB - dateA == 0) return b.hora - a.hora;
-        else return dateB - dateA;
-      });
+      // const markets = action.payload.sort((a, b) => {
+      //   let dateA = new Date(a.fecha),
+      //     dateB = new Date(b.fecha);
+      //   if (dateB - dateA == 0) return b.hora - a.hora;
+      //   else return dateB - dateA;
+      // });
+      const markets = action.payload.reverse()
       return { ...state, all: markets };
 
     default:
